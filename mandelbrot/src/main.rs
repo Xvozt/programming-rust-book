53d7532d5ffca9bc014611ac5a1db8493eeb3a12
Sergey Kupriyanov <xvoztuk@gmail.com>
2025-07-06 22:29:15 +0400

    start example of mandelbrot set

---
+++ mandelbrot/src/main.rs
@@ -0,0 +1,18 @@
+use num::Complex;
+
+fn main() {
+    println!("Hi mandelbrot!");
+}
+
+fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
+    let mut z = Complex { re: 0.0, im: 0.0 };
+
+    for i in 0..limit {
+        if z.norm_sqr() > 4.0 {
+            return Some(i);
+        }
+        z = z * z + c;
+    }
+
+    None
+}
