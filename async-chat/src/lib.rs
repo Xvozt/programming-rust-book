ec48cc9a19f7bcb8ccac3ee3c6ba36faff7243bc
Sergey Kupriyanov <xvoztuk@gmail.com>
2026-02-12 00:10:58 +0400

    base types for chat

--- async-chat/src/lib.rs
+++ async-chat/src/lib.rs
@@ -1,5 +1,26 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::sync::Arc;
+
+use serde::{Deserialize, Serialize};
+
+pub mod utils;
+
+#[derive(Debug, Deserialize, Serialize, PartialEq)]
+pub enum FromClient {
+    Join {
+        group_name: Arc<String>,
+    },
+    Post {
+        group_name: Arc<String>,
+        message: Arc<String>,
+    },
+}
+
+pub enum FromServer {
+    Message {
+        group_name: Arc<String>,
+        message: Arc<String>,
+    },
+    Error(String),
 }
 
 #[cfg(test)]
@@ -7,8 +28,21 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_from_client_json() {
+        let from_client = FromClient::Post {
+            group_name: Arc::new("Dogs".to_string()),
+            message: Arc::new("Pitbulls here".to_string()),
+        };
+
+        let json = serde_json::to_string(&from_client).unwrap();
+
+        assert_eq!(
+            json,
+            r#"{"Post":{"group_name":"Dogs","message":"Pitbulls here"}}"#
+        );
+        assert_eq!(
+            serde_json::from_str::<FromClient>(&json).unwrap(),
+            from_client
+        )
     }
 }
