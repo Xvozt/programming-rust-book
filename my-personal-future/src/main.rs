c21fd24b01b74e9a81f76f77b82908a0bf90c9b9
s.v.kupriyanov <s.v.kupriyanov@tbank.ru>
2026-02-23 13:45:52 +0400

    implementation of a future and simple executor

---
+++ my-personal-future/src/main.rs
@@ -0,0 +1,91 @@
+use std::{
+    sync::{Arc, Mutex},
+    task::{Context, Poll, Waker},
+    thread,
+    time::Duration,
+};
+
+use crossbeam::sync::Parker;
+use futures_lite::pin;
+use waker_fn::waker_fn;
+futures_lite::pin!();
+
+pub fn spawn_blocking<T, F>(closure: F) -> SpawnBlocking<T>
+where
+    F: FnOnce() -> T,
+    F: Send + 'static,
+    T: Send + 'static,
+{
+    let inner = Arc::new(Mutex::new(Shared {
+        value: None,
+        waker: None,
+    }));
+
+    std::thread::spawn({
+        let inner = inner.clone();
+        move || {
+            let value = closure();
+
+            let maybe_waker = {
+                let mut guard = inner.lock().unwrap();
+                guard.value = Some(value);
+                guard.waker.take()
+            };
+
+            if let Some(waker) = maybe_waker {
+                waker.wake();
+            }
+        }
+    });
+
+    SpawnBlocking(inner)
+}
+
+pub struct SpawnBlocking<T>(Arc<Mutex<Shared<T>>>);
+
+struct Shared<T> {
+    value: Option<T>,
+    waker: Option<Waker>,
+}
+
+impl<T: Send> Future for SpawnBlocking<T> {
+    type Output = T;
+
+    fn poll(self: std::pin::Pin<&mut Self>, cx: &mut std::task::Context<'_>) -> std::task::Poll<T> {
+        let mut guard = self.0.lock().unwrap();
+        if let Some(value) = guard.value.take() {
+            return Poll::Ready(value);
+        }
+
+        guard.waker = Some(cx.waker().clone());
+        Poll::Pending
+    }
+}
+
+pub fn block_on<F: Future>(future: F) -> F::Output {
+    let parker = Parker::new();
+    let unparker = parker.unparker().clone();
+    let waker = waker_fn(move || unparker.unpark());
+    let mut context = Context::from_waker(&waker);
+    pin!(future);
+
+    loop {
+        match future.as_mut().poll(&mut context) {
+            Poll::Ready(value) => return value,
+            Poll::Pending => parker.park(),
+        }
+    }
+}
+
+fn main() {
+    let expensive_closure = || -> u32 {
+        println!("Calculating slowly...");
+        thread::sleep(Duration::from_secs(3));
+        5
+    };
+
+    let future = spawn_blocking(expensive_closure);
+
+    let block = block_on(future);
+    println!("{}", block)
+}
